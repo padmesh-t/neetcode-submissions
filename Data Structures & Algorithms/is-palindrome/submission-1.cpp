224e26496572329e2e788e69c924a2b2e06040bb
Padmesh T <187275427+padmesh-t@users.noreply.github.com>
2026-06-23 14:41:57 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int l = 0 , r = s.size()-1 ; l < r ; l++ , r-- ){
+            while( l < r && !( (s[l] >= 'A' && s[l] <= 'Z' ) || ( s[l] >= 'a' && s[l] <= 'z') || ( s[l] >= '0' && s[l] <= '9' ) )){
+                l++;
+            }
+            while( l < r && !( (s[r] >= 'A' && s[r] <= 'Z' ) || ( s[r] >= 'a' && s[r] <= 'z') || ( s[r] >= '0' && s[r] <= '9' ) )){
+                r--;
+            }
+            if( l < r && tolower(s[l]) != tolower(s[r]) ) return false;
+        }
+        return true;
+    }
+};
